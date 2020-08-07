1cd464a4b3a59374c43efc6724e439323122c96b
warriorkid8677 <duitnn@gmail.com>
2020-08-07 18:19:45 -0500

    Added my Android Studio Project with limited but working mapbox app.

---
+++ MyMapbox/app/src/main/java/com/hackathon2020/mymapbox/MainActivity.java
@@ -0,0 +1,39 @@
+package com.hackathon2020.mymapbox;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
+        setContentView(R.layout.activity_main);
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded(){
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style){
+
+                    }
+                });
+            }
+        });
+    }
+}
