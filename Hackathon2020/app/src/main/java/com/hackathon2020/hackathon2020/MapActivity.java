57598fa48e39d659f6754e639d115d39fcb339e0
warriorkid8677 <duitnn@gmail.com>
2020-08-08 15:01:51 -0500

    Committed back button functionality, added a map when clicking display map.

--- Hackathon2020/app/src/main/java/com/hackathon2020/hackathon2020/MapActivity.java
+++ Hackathon2020/app/src/main/java/com/hackathon2020/hackathon2020/MapActivity.java
@@ -1,14 +1,53 @@
 package com.hackathon2020.hackathon2020;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
 
 public class MapActivity extends AppCompatActivity {
+
+    private MapView mapView;
+    private Button backButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.MAPBOX_ACCESS_TOKEN));
         setContentView(R.layout.activity_map);
+        backButton = (Button) findViewById(R.id.BackButtonMap);
+
+        backButton.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MapActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.SATELLITE_STREETS, new Style.OnStyleLoaded(){
+
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+                    }
+                });
+            }
+        });
     }
 }
